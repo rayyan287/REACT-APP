7784459e9f4859fe041f36e358c8a9357d888307
rayyan287 <hafizmuhammadrayyan324.com>
2025-07-07 05:30:28 +0500

    8thone

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import LandingPage from "./components/Landingpage";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 
 function App() {
   return (
